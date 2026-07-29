6f0fab237ef7445ccd1218e21fcb58479c9d174b
Jalik-aaa <jalik.99@mail.ru>
2026-07-29 11:03:28 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'equip-tracker-v7';
+const CACHE = 'equip-tracker-v8';
 const FILES = ['./index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', e => {
